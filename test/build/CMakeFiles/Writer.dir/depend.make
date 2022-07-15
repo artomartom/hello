1e63c0cdd2de05fc2554f19f119ace0997c27c26
artom artom <1tematematema@gmail.com>
2022-07-15 20:55:38 +0300

    .gitign...

--- test/build/CMakeFiles/Writer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Writer.
-# This may be replaced when dependencies are built.
--- test/build/CMakeFiles/window.dir/window.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/window.dir/window.cpp.o: \
- /home/artom/deve/projects/hello/test/window.cpp \
- /usr/include/stdc-predef.h
